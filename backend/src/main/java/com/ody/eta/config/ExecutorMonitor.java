0b838aa603620cce2ceaf75d73c137f8459c8632
coli <kkwoo001021@naver.com>
2025-09-14 18:45:56 +0900

    feat: 풀 사태 로깅 모니터 설정 구성

---
+++ backend/src/main/java/com/ody/eta/config/ExecutorMonitor.java
@@ -0,0 +1,31 @@
+package com.ody.eta.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Slf4j
+@Configuration
+@EnableScheduling
+public class ExecutorMonitor {
+
+    private final ThreadPoolTaskExecutor executor;
+
+    public ExecutorMonitor(@Qualifier("routeTimeCallExecutor") ThreadPoolTaskExecutor executor) {
+        this.executor = executor;
+    }
+
+    @Scheduled(fixedRate = 1_000) // 1초마다 실행
+    public void logExecutorStatus() {
+        var pool = executor.getThreadPoolExecutor();
+        log.info("[ExecutorStatus] PoolSize={}, ActiveCount={}, CompletedTaskCount={}, QueueSize={}",
+                pool.getPoolSize(),
+                pool.getActiveCount(), //실행중인 작업 수
+                pool.getCompletedTaskCount(), //완료 한 작업
+                pool.getQueue().size()); //대기 중인 작업
+    }
+}
+
