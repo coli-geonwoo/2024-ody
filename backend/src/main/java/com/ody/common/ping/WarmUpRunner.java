c1d273ea301699b26eeb31f0808b7885feb7008b
coli <kkwoo001021@naver.com>
2025-09-16 19:38:51 +0900

    feat: client 순서 변경

--- backend/src/main/java/com/ody/common/ping/WarmUpRunner.java
+++ backend/src/main/java/com/ody/common/ping/WarmUpRunner.java
@@ -1,5 +1,8 @@
 package com.ody.common.ping;
 
+import com.ody.eta.dto.request.MateEtaRequest;
+import com.ody.eta.service.EtaService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -10,15 +13,18 @@ import org.springframework.stereotype.Component;
 public class WarmUpRunner {
 
     private final ThreadPoolTaskExecutor executor;
+    private final EtaService etaService;
 
     public WarmUpRunner(
-            @Qualifier("routeTimeCallExecutor") ThreadPoolTaskExecutor executor
+            @Qualifier("routeTimeCallExecutor") ThreadPoolTaskExecutor executor,
+            EtaService etaService
     ) {
         this.executor = executor;
+        this.etaService = etaService;
     }
 
     public void run() {
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 50; i++) {
             try {
                 executor.execute(() -> {
                     log.info("WarmUpRunner run start");
