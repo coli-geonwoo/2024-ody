ed14295923ce00bebe2c5a4299fa85bad82f94a8
coli <kkwoo001021@naver.com>
2025-09-19 12:29:25 +0900

    feat: 외부 api를 트랜잭션에 포함

--- backend/src/main/java/com/ody/eta/service/EtaService.java
+++ backend/src/main/java/com/ody/eta/service/EtaService.java
@@ -91,6 +91,7 @@ public class EtaService {
     }
 
     @Async("routeTimeCallExecutor")
+    @Transactional
     @EventListener(UpdateRouteTimeEvent.class)
     public void updateByRouteTimeCall(UpdateRouteTimeEvent event) {
         Coordinates origin = event.getOrigin();
