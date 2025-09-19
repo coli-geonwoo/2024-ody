74cdae518ce6488019e16ce9fb1aa5a30ba44ba5
coli <kkwoo001021@naver.com>
2025-09-19 11:09:48 +0900

    chore: 스레드풀 설정 변경

--- backend/src/main/java/com/ody/eta/config/RouteTimeCallTaskExecutorConfig.java
+++ backend/src/main/java/com/ody/eta/config/RouteTimeCallTaskExecutorConfig.java
@@ -17,8 +17,8 @@ public class RouteTimeCallTaskExecutorConfig {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         int coreCount = Runtime.getRuntime().availableProcessors();
         executor.setCorePoolSize(20);
-        executor.setMaxPoolSize(100);
-        executor.setQueueCapacity(50);
+        executor.setMaxPoolSize(120);
+        executor.setQueueCapacity(0);
         executor.setKeepAliveSeconds(30);
         executor.setThreadNamePrefix("route-time-call-task-executor-");
         executor.setRejectedExecutionHandler(new java.util.concurrent.ThreadPoolExecutor.CallerRunsPolicy());
